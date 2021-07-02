b377b9f03556d2cc1b83fb29d41e5df52846c2ba
Muskan <muskan80525@gmail.com>
2021-07-02 22:48:42 +0530

    Add register feature and database along with register and login template

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,flash,session,url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import pytz
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'sqlite:///database.db'
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/?'
 db = SQLAlchemy(app)
 
 IST = pytz.timezone('Asia/Kolkata')
@@ -36,6 +36,42 @@ class Task(db.Model):
 def home():
     return "<h1>Task Manager</h1>"
 
+@app.route('/register',methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        user_name = request.form['user_name']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password == confirm_password:
+            if email != "" and password != "":
+                existing_user = User.query.filter_by(email = email).first()
+                if existing_user == None:
+                    user = User(user_name = user_name,email = email,password = password)
+                    db.session.add(user)
+                    db.session.commit()
+                    message = "Account Created Successfully!" 
+                    # status = "success"
+                    flash(message)
+                    return redirect(url_for('login')) 
+                else:
+                    message = "Account already exists."
+                    flash(message)
+                    return redirect(url_for('register'))           
+            else:
+                message = "Please enter email and password!!"
+                flash(message)
+                return redirect(url_for('register'))       
+        else:
+            message = "Password didn't match" 
+            flash(message)
+            return redirect(url_for('register'))          
+    else:
+        return render_template('register.html')
+
+@app.route('/login',methods =['POST','GET'])
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
